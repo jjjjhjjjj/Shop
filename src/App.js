45e8653a232338bfe5bae8d18164ab210e307969
Hj <jjjhjjjjj123@gmail.com>
2023-03-29 16:24:54 +0900

    Feat: 기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Shoppy</div>;
+  return (
+    <>
+      <h1>Shoppy</h1>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import AllProducts from "./pages/AllProducts";
+import NewProduct from "./pages/NewProduct";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
+import App from "./App";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, path: "/", element: <div>list</div> },
+      {
+        path: "/products",
+        element: <AllProducts />,
+      },
+      {
+        path: "/products/new",
+        element: <NewProduct />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetail />,
+      },
+      {
+        path: "/carts",
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
