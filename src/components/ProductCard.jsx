60138371bfa9512974b714f85b55d421cbfc29c0
Hj <jjjhjjjjj123@gmail.com>
2023-03-30 16:40:32 +0900

    Feat: 상품 카드 구현

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,14 @@
-export default function ProductCard({ product }) {
-  return <li>{product.name}</li>;
+export default function ProductCard({
+  product: { name, img, price, category },
+}) {
+  return (
+    <li>
+      <img src={img} alt="" />
+      <div>
+        <p>{name}</p>
+        <p>{price}</p>
+      </div>
+      <span>{category}</span>
+    </li>
+  );
 }
