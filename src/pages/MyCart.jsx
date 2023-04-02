190aa64bd0d2193f71b15054d3318470a7b4d9a5
Hj <jjjhjjjjj123@gmail.com>
2023-04-03 01:45:50 +0900

    장바구니 총 금액 구현

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -3,6 +3,8 @@ import { useAuth } from "../context/authContext";
 import { getCart } from "../api/firebase";
 import CartItem from "../components/CartItem";
 
+const SHIP_PRICE = 3000;
+
 export default function MyCart() {
   const { user } = useAuth();
   const [carts, setCarts] = useState();
@@ -11,14 +13,14 @@ export default function MyCart() {
     getCart(user.uid).then((data) => setCarts(Object.values(data)));
   }, []);
 
-  // const totalPrice =
-  //   carts &&
-  //   Object.values(carts).reduce(
-  //     (prev, current) => prev + parseInt(current.price) * current.quantity,
-  //     0
-  //   );
+  const totalPrice =
+    carts &&
+    carts.reduce(
+      (prev, current) => prev + parseInt(current.price) * current.count,
+      0
+    );
 
-  // console.log(totalPrice);
+  console.log(totalPrice);
   return (
     <section>
       {!carts && <p>장바구니에 상품이 없습니다.</p>}
@@ -29,6 +31,11 @@ export default function MyCart() {
           ))}
         </ul>
       )}
+      <ul>
+        <li>{totalPrice}</li>
+        <li>{SHIP_PRICE}</li>
+        <li>{totalPrice + SHIP_PRICE}</li>
+      </ul>
     </section>
   );
 }
